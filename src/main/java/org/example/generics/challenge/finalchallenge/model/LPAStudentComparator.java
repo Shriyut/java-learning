a96f5c8fc671a3766414c85953c0acffdb55d5a6
shriyut <shriyutjha21@gmail.com>
2025-02-24 18:56:50 +0530

    Generics v4

---
+++ src/main/java/org/example/generics/challenge/finalchallenge/model/LPAStudentComparator.java
@@ -0,0 +1,11 @@
+package org.example.generics.challenge.finalchallenge.model;
+
+import java.util.Comparator;
+
+public class LPAStudentComparator implements Comparator<LPAStudent> {
+
+    @Override
+    public int compare(LPAStudent o1, LPAStudent o2) {
+        return (int) (o1.getPercentComplete() - o2.getPercentComplete());
+    }
+}
---
+++ src/main/java/org/example/generics/challenge/finalchallenge/util/QueryItem.java
@@ -0,0 +1,6 @@
+package org.example.generics.challenge.finalchallenge.util;
+
+public interface QueryItem {
+
+    public boolean matchFieldValue(String fieldName, String value);
+}
